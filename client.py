ad578785dca7c57f652c534b21853ae90bdd6967
Dea María Léon <deamarialeon@gmail.com>
2023-10-28 16:38:38 +0200

    Added run_reason to client.py

--- client.py
+++ client.py
@@ -6,11 +6,15 @@ os.environ["CONBENCH_URL"] = "http://127.0.0.1:5000"
 os.environ["CONBENCH_EMAIL"] = "deamarialeon@gmail.com"
 os.environ["CONBENCH_PASSWORD"] = "x"
 os.environ["CONBENCH_PROJECT_REPOSITORY"] = "git@github.com:DeaMariaLeon/algos2.git"
+os.environ["CONBENCH_RUN_REASON"] = "first"
 #os.environ[""]
 #"github": "https://github.com/DeaMariaLeon/algos2.git"
 adapter = AsvBenchmarkAdapter(
     command=["echo", "'Hello, world!'"],
     result_dir=Path(__name__).parent.absolute(),
+    result_fields_override={
+        "run_reason": os.getenv("CONBENCH_RUN_REASON")
+    },
 )
 
 adapter.run()
