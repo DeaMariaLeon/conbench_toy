89c8ae8b536967bb5367059edce5356d6f332a58
Dea María Léon <deamarialeon@gmail.com>
2023-10-29 16:37:15 +0100

    using real commit number

--- client.py
+++ client.py
@@ -5,11 +5,11 @@ import os
 os.environ["CONBENCH_URL"] = "http://127.0.0.1:5000"
 os.environ["CONBENCH_EMAIL"] = "deamarialeon@gmail.com"
 os.environ["CONBENCH_PASSWORD"] = "x"
-os.environ["CONBENCH_PROJECT_REPOSITORY"] = "git@github.com:DeaMariaLeon/algos2.git"
+os.environ["CONBENCH_PROJECT_REPOSITORY"] = "git@github.com:DeaMariaLeon/algos2"
 os.environ["CONBENCH_RUN_REASON"] = "first"
 #
 os.environ["CONBENCH_PROJECT_PR_NUMBER"] = "1"
-os.environ["CONBENCH_PROJECT_COMMIT"] = "commit"
+os.environ["CONBENCH_PROJECT_COMMIT"] = "cb63287e63db10ce3eb0f3b8c279fd995678d0ae"
 #"github": "https://github.com/DeaMariaLeon/algos2.git"
 adapter = AsvBenchmarkAdapter(
     command=["echo", "'Hello, world!'"],
@@ -26,7 +26,4 @@ adapter = AsvBenchmarkAdapter(
 adapter.run()
 #adapter()
 print(adapter.results)
-#adapter.results.post_results() # AttributeError: 'AsvBenchmarkAdapter' object has no attribute 'publish_results'. Did you mean: 'post_results'?
-#adapter.publish_results()
-#adapter()
 adapter.post_results()
