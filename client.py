1c0cdd07f224641feb691bbda4abd6b24b9a7780
Dea María Léon <deamarialeon@gmail.com>
2023-10-29 10:47:05 +0100

    changed benchmarks.json

--- client.py
+++ client.py
@@ -7,7 +7,9 @@ os.environ["CONBENCH_EMAIL"] = "deamarialeon@gmail.com"
 os.environ["CONBENCH_PASSWORD"] = "x"
 os.environ["CONBENCH_PROJECT_REPOSITORY"] = "git@github.com:DeaMariaLeon/algos2.git"
 os.environ["CONBENCH_RUN_REASON"] = "first"
-#os.environ[""]
+#
+os.environ["CONBENCH_PROJECT_PR_NUMBER"] = "1"
+os.environ["CONBENCH_PROJECT_COMMIT"] = "commit"
 #"github": "https://github.com/DeaMariaLeon/algos2.git"
 adapter = AsvBenchmarkAdapter(
     command=["echo", "'Hello, world!'"],
@@ -15,6 +17,10 @@ adapter = AsvBenchmarkAdapter(
     result_fields_override={
         "run_reason": os.getenv("CONBENCH_RUN_REASON")
     },
+    #result_fields_append={
+    #    "info": {"build_version": os.getenv("MY_BUILD_VERSION")},
+    #    "context": {"compiler_flags": os.getenv("MY_COMPILER_FLAGS")}
+    #},
 )
 
 adapter.run()
