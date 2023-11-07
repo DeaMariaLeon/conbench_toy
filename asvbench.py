2746325223e1e5483c65089cc0603efe41652f1f
Dea María Léon <deamarialeon@gmail.com>
2023-11-07 18:45:20 +0100

    finding errors on source files

--- asvbench.py
+++ asvbench.py
@@ -49,33 +49,29 @@ class AsvBenchmarkAdapter(BenchmarkAdapter):
         
         parsed_benchmarks = []
         
-        #with open("a83f6aae-pandas2.json") as f:
+        
         with open("pandas3-2benchmarks.json") as f:
         #with open("c2cdeaf3-env-36436ace7d7eead1c76ef118fd27f1fa.json") as f:
+        #with open("6493d2a4-env-36436ace7d7eead1c76ef118fd27f1fa.json") as f:
             benchmarks_results = json.load(f)
         
         with open("benchmarks.json") as f:
             benchmarks_info = json.load(f)
-        
-
         names = benchmarks_results["results"].keys()
         no_results = []
-        for n in names:
-            #bench = dict(zip(benchmarks_results["result_columns"], 
-            #                 benchmarks_results["results"][n]))
+        for n in names:       
             param_names = benchmarks_info[n]['param_names']
             param_values = benchmarks_info[n]['params']
             combinations = [p for p in itertools.product(*param_values)]
-        
+           
             for i in range(len(combinations)):
-                param_dic = dict(zip(param_names,combinations[i]))
-                
-
+                param_dic = dict(zip(param_names,combinations[i]))                
                 tags = {}
                 tags["name"] = n
                 tags.update(param_dic)
 
                 if (benchmarks_results["results"][n][0]):
+                    
                     data = [benchmarks_results["results"][n][0][i]]
                     parsed_benchmark = BenchmarkResult(
                         batch_id="A",
@@ -93,12 +89,11 @@ class AsvBenchmarkAdapter(BenchmarkAdapter):
                                 },
                         
                     )
-                    print(data)
                     parsed_benchmarks.append(parsed_benchmark)
                     
                 else:
                     no_results.append(benchmarks_results["results"][n])
-                
-        print(no_results)
+            exit
+        #print(no_results)
         return parsed_benchmarks     
 
