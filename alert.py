544a481fe87a2de5a411d34a2fecb16251b601e3
Dea María Léon <deamarialeon@gmail.com>
2023-12-28 10:29:30 +0100

    before merge

--- alert.py
+++ alert.py
@@ -10,17 +10,18 @@ from benchalerts.conbench_dataclasses import FullComparisonInfo
 
 load_dotenv(dotenv_path="./local_env.yml")
 
-# Pretend we're running on GitHub Actions. Get some environment variables.
-# (they may be different for different CI systems)
 #commit_hash = os.environ["GITHUB_SHA"]
 commit_hash = "c8a9c2fd3bcf23a21acfa6f4cffbc4c9360b9ea6"
+#commit_hash = "007310665f8e2741ac5694f05d9412bbe6e326e8"
+
 repo = os.getenv("GITHUB_REPOSITORY")
 
-build_url = (
-    "https://github.com" #os.environ["GITHUB_SERVER_URL"]
-    + f"/{repo}/actions/runs/"
-    + "7289196759" #os.environ["GITHUB_RUN_ID"]
-)
+#build_url = (
+#    "https://github.com" #os.environ["GITHUB_SERVER_URL"]
+#    + f"/{repo}/actions/runs/"
+#    #+ "7289196759" #os.environ["GITHUB_RUN_ID"]
+#    + "7323312613"
+#)
 
 # Create a pipeline to update a GitHub Check
 pipeline = AlertPipeline(
@@ -28,23 +29,24 @@ pipeline = AlertPipeline(
         steps.GetConbenchZComparisonStep(
             commit_hash=commit_hash,
             #baseline_run_type=steps.BaselineRunCandidates.fork_point,
+            #baseline_run_type=steps.BaselineRunCandidates.latest_default,
             baseline_run_type=steps.BaselineRunCandidates.parent,
-            #z_score_threshold=-1,
+            z_score_threshold=1, #If not set it defaults to 5
         ),
         steps.GitHubCheckStep(
             commit_hash=commit_hash,
             comparison_step_name="GetConbenchZComparisonStep",
             github_client=GitHubRepoClient(repo=repo),
-            build_url=build_url
-            #alerter=Alerter.github_check_details(full_comparison)
+            #build_url=build_url,
         ),
     ],
     error_handlers=[
         steps.GitHubCheckErrorHandler(
-            commit_hash=commit_hash, repo=repo, build_url=build_url
+            commit_hash=commit_hash, repo=repo, #build_url=build_url
         )
     ],
 )
 
 # Run the pipeline
 print(pipeline.run_pipeline())
+
