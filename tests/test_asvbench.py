d4bf74e77360e4ebe937eb784638d61de316fe89
Dea María Léon <deamarialeon@gmail.com>
2024-03-04 18:47:39 +0100

    adding test for converter

--- tests/test_asvbench.py
+++ tests/test_asvbench.py
@@ -1,10 +1,12 @@
 import json
+import tempfile
+from pathlib import Path
 import pytest
 
 from asvbench import AsvBenchmarkAdapter
 from benchadapt.result import BenchmarkResult
 
-ASV_RESULT = {
+asv_json = {
     "commit_hash": "0d24f201cc2506de45a2ed688d9f72f7123d58f7",
     "env_name": "conda-py3.11",
     "date": 1703062645000,
@@ -68,7 +70,7 @@ ASV_RESULT = {
     "version": 2
 }
 
-BENCHMARK_JSON = {
+benchmarks_json = {
     "benchmarks.TimeSuite.time_insertion_sort": {
         "code": "class TimeSuite:\n    def time_insertion_sort(self):\n        sort(self.array)\n\n    def setup(self):\n        LENGTH = 10\n        #random.seed(10)\n        self.array = [random.randint(0, 1000) for i in range(LENGTH)]",
         "min_run_count": 2,
@@ -86,3 +88,36 @@ BENCHMARK_JSON = {
     },
     "version": 2
 }
+
+
+class TestAsvAdapter:
+    @pytest.fixture
+    def asv_adapter(self, monkeypatch):
+        monkeypatch.setenv(
+        
+            "REPOSITORY", "git@github.com:conchair/conchair"
+        )
+        
+        result_file = tempfile.mktemp(suffix=".json")
+        tempdir =  Path(tempfile.mkdtemp())
+        
+
+        asv_adapter = AsvBenchmarkAdapter(
+            command=["echo", "'Hello, world!'"], 
+            result_file=Path(result_file),
+            benchmarks_file_path=str(tempdir)+"/",
+        )
+
+        with open(asv_adapter.result_file, "w") as f:
+            json.dump(asv_json, f)
+        
+        name = tempdir.joinpath("benchmarks.json")
+        with open(name, "w") as f:
+            json.dump(benchmarks_json, f)
+
+        return asv_adapter
+    
+    def test_transform_results(self, asv_adapter) -> None:
+        results = asv_adapter.transform_results()
+
+        assert len(results) == 1
