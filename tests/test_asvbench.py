de30ab4c2c16fff55f2f77b078347e080c3cdaa7
Dea María Léon <deamarialeon@gmail.com>
2024-03-05 11:03:18 +0100

    adding to test

--- tests/test_asvbench.py
+++ tests/test_asvbench.py
@@ -8,18 +8,18 @@ from benchadapt.result import BenchmarkResult
 
 asv_json = {
     "commit_hash": "0d24f201cc2506de45a2ed688d9f72f7123d58f7",
-    "env_name": "conda-py3.11",
+    "env_name": "conda-py3.12",
     "date": 1703062645000,
     "params": {
         "arch": "arm64",
         "cpu": "Apple M2",
-        "machine": "Deas-MacBook-Air.local",
+        "machine": "MyMachine",
         "num_cpu": "8",
         "os": "macOS 13.5.1 (22G90)",
         "ram": "16GB",
-        "python": "3.11"
+        "python": "3.12"
     },
-    "python": "3.11",
+    "python": "3.12",
     "requirements": {},
     "env_vars": {},
     "result_columns": [
@@ -72,7 +72,7 @@ asv_json = {
 
 benchmarks_json = {
     "benchmarks.TimeSuite.time_insertion_sort": {
-        "code": "class TimeSuite:\n    def time_insertion_sort(self):\n        sort(self.array)\n\n    def setup(self):\n        LENGTH = 10\n        #random.seed(10)\n        self.array = [random.randint(0, 1000) for i in range(LENGTH)]",
+        "code": "class TimeSuite:\n    def time_insertion_sort(self):\n        sort(self.array)\n\n    def setup(self):\n        LENGTH = 10\n        self.array = [random.randint(0, 1000) for i in range(LENGTH)]",
         "min_run_count": 2,
         "name": "benchmarks.TimeSuite.time_insertion_sort",
         "number": 4,
@@ -103,7 +103,7 @@ class TestAsvAdapter:
         
 
         asv_adapter = AsvBenchmarkAdapter(
-            command=["echo", "'Hello, world!'"], 
+            command=["echo", "'Hello, world!'"],
             result_file=Path(result_file),
             benchmarks_file_path=str(tempdir)+"/",
         )
@@ -119,5 +119,22 @@ class TestAsvAdapter:
     
     def test_transform_results(self, asv_adapter) -> None:
         results = asv_adapter.transform_results()
-
+        print(results)
         assert len(results) == 1
+        assert len(set(res.tags["name"] for res in results)) == 1
+        assert len(set(res.batch_id for res in results)) == 1
+        for result in results:
+        
+            assert isinstance(result, BenchmarkResult)
+            assert isinstance(result.run_name, str)
+            assert result.tags["name"].endswith("time_insertion_sort")
+            assert result.context == {'benchmark_language': 'Python',
+                                      'env_name': 'conda-py3.12',
+                                      'python': '3.12',
+                                      'requirements': {}}
+            
+            assert result.machine_info is not None
+            assert result.stats == {'data': [1.2035623905003376], 
+                                    'unit': 's', 
+                                    'iterations': 1}
+            assert result.github['commit'] == '0d24f201cc2506de45a2ed688d9f72f7123d58f7'
